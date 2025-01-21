538a18f0da2876c27a329cd09ce2e96eafeda5fb
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-01-21 13:39:59 +0100

    Add discount styling and toggle cover visibility in book list</message>
    <message>
    - Introduced CSS classes for discount and expensive book pricing in book-list.component.css.
    - Added a button to toggle the visibility of book covers in book-list.component.html.
    - Updated book-list.component.ts to manage cover visibility state and adjusted book data to include cover URLs.

--- books/src/app/books/book-list/book-list.component.ts
+++ books/src/app/books/book-list/book-list.component.ts
@@ -7,14 +7,23 @@ import { Component } from '@angular/core';
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
+  bildBreite = 50;
+
+  coverIsVisible = true;
+
   public books =  [
-    {isbn: '1234567890', title: 'Buch1', price: 10},
-    {isbn: '1234567891', title: 'Buch2', price: 20},
-    {isbn: '1234567892', title: 'Buch3', price: 30}
-  ];
+    {isbn: '1234567890', title: 'Buch1', price: 10, coverUrl:'https://m.media-amazon.com/images/I/71Wv+d6oP6L._AC_UY218_.jpg'},
+    {isbn: '1234567891', title: 'Buch2', price: 20, coverUrl:'https://m.media-amazon.com/images/I/71wlgd2ShsL._AC_UY218_.jpg'},
+    {isbn: '1234567892', title: 'Buch3', price: 30, coverUrl:'https://m.media-amazon.com/images/I/61l7nyf3OmL._AC_UY218_.jpg'},
+    ];
 
   trackByBook(index: number, book: any) {
     return book.isbn;
+  }
+
+
+  toggleCover() {
+    this.coverIsVisible = !this.coverIsVisible;
   }
 
   public autos = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
