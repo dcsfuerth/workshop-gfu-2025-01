bb36ea38dedd1bff4a6d06c83546e15b93a5f211
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-01-23 14:52:22 +0100

    Enhance RxJsSpielwiese component with observables and async data handling
    - Updated rx-js-spielwiese.component.ts to implement OnInit lifecycle and added observable streams for cent and euro values.
    - Introduced async pipe in rx-js-spielwiese.component.html to display euro values dynamically.
    - Implemented a Subject to demonstrate multiple subscribers and data emission.
    
    These changes improve the functionality of the RxJsSpielwiese component, showcasing reactive programming with RxJS.

--- books/src/app/shared/rx-js-spielwiese/rx-js-spielwiese.component.ts
+++ books/src/app/shared/rx-js-spielwiese/rx-js-spielwiese.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { filter, map, of, Subject, timer } from 'rxjs';
 
 @Component({
-  selector: 'books-rx-js-spielwiese',
   standalone: false,
-  
   templateUrl: './rx-js-spielwiese.component.html',
-  styleUrl: './rx-js-spielwiese.component.css'
+  styleUrl: './rx-js-spielwiese.component.css',
 })
-export class RxJsSpielwieseComponent {
+export class RxJsSpielwieseComponent implements OnInit {
+  obsCent$ = timer(0, 1000);
+  obsEuro$ = of(0);
 
+  ngOnInit() {
+    this.obsEuro$ = this.obsCent$.pipe(
+      filter((cent: number) => cent % 10 === 0),
+      map((cent) => (cent + 0.0) / 100)
+    );
+
+    this.obsEuro$.subscribe(console.log);
+
+    const mySubject$ = new Subject();
+    mySubject$.next('Jemand zuhause?');
+    mySubject$.subscribe((e) => console.log('Zuhörer 1', e));
+    mySubject$.subscribe((e) => console.log('Zuhörer 2', e));
+    mySubject$.next('Hallo');
+    mySubject$.next('Welt :-) ');
+
+    this.obsEuro$.subscribe((data) => {
+      mySubject$.next(data);
+    });
+  }
 }
