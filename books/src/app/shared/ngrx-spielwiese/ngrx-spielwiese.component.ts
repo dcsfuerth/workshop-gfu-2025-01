5dcdf44f17344fe0a3a4ab2d44ec7ecaf63832c9
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-01-24 15:40:44 +0100

    Enhance ngrx-spielwiese component with new action and button for setting counter
    - Added a new action creator `setCounterCreator` to allow setting the counter to a specific value.
    - Introduced a new button in the HTML template to trigger the `setze42` method, which dispatches the action to set the counter to 42.
    - Updated the `app.module.ts` to increase the maxAge for StoreDevtoolsModule from 25 to 100, improving state history tracking during development.
    
    These changes improve the functionality of the ngrx-spielwiese component and enhance the debugging experience with better state management.

--- books/src/app/shared/ngrx-spielwiese/ngrx-spielwiese.component.ts
+++ books/src/app/shared/ngrx-spielwiese/ngrx-spielwiese.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
-import { createAction, Store } from '@ngrx/store';
+import { createAction, props, Store } from '@ngrx/store';
 import { createReducer, on } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 
-export const initialState = 0;
+export const initialState: number = 0; // {book: Book[], customers: Customer[]}
 export const incrementCreator = createAction('[Counter] Increment');
 export const decrementCreator = createAction('[Counter] Decrement');
 export const resetCreator = createAction('[Counter] Reset');
+export const setCounterCreator = createAction(
+  '[Counter] Set',
+  props<{ count: number }>()
+);
 
 export const counterReducer = createReducer(
   initialState,
   on(incrementCreator, (state) => state + 1),
   on(decrementCreator, (state) => state - 1),
-  on(resetCreator, (state) => 0)
+  on(resetCreator, (state) => 0),
+  on(setCounterCreator, (state, action) => action.count)
 );
 
 @Component({
@@ -28,8 +33,7 @@ export class NgrxSpielwieseComponent {
   }
 
   increment() {
-    const actionIncrement = incrementCreator();
-    this.store.dispatch(actionIncrement);
+    this.store.dispatch(incrementCreator());
   }
   decrement() {
     this.store.dispatch(decrementCreator());
@@ -37,5 +41,8 @@ export class NgrxSpielwieseComponent {
   }
   reset() {
     this.store.dispatch(resetCreator());
+  }
+  setze42() {
+    this.store.dispatch(setCounterCreator({ count: 42 }));
   }
 }
