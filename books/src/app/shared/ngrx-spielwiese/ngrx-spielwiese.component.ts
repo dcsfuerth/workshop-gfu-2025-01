362690941d12d027cd53f088463b75c0a78c09e1
Peter Dickten <peter.dickten@dcs-fuerth.de>
2025-01-24 13:25:25 +0100

    Refactor ngrx-spielwiese component for improved state management and action creators
    - Updated action creators to use more descriptive names (incrementCreator, decrementCreator, resetCreator) for clarity.
    - Enhanced the constructor to inject the Store, allowing for better state management and dispatching of actions.
    - Improved the increment, decrement, and reset methods to dispatch the newly named actions, streamlining the component's interaction with the store.
    
    These changes enhance the maintainability and readability of the ngrx-spielwiese component, contributing to a more robust state management implementation.

--- books/src/app/shared/ngrx-spielwiese/ngrx-spielwiese.component.ts
+++ books/src/app/shared/ngrx-spielwiese/ngrx-spielwiese.component.ts
@@ -1,40 +1,41 @@
 import { Component } from '@angular/core';
-import { createAction } from '@ngrx/store';
+import { createAction, Store } from '@ngrx/store';
 import { createReducer, on } from '@ngrx/store';
 import { Observable, of } from 'rxjs';
 
 export const initialState = 0;
-export const increment = createAction('[Counter] Increment');
-export const decrement = createAction('[Counter] Decrement');
-export const reset = createAction('[Counter] Reset');
+export const incrementCreator = createAction('[Counter] Increment');
+export const decrementCreator = createAction('[Counter] Decrement');
+export const resetCreator = createAction('[Counter] Reset');
 
 export const counterReducer = createReducer(
   initialState,
-  on(increment, (state) => state + 1),
-  on(decrement, (state) => state - 1),
-  on(reset, (state) => 0)
+  on(incrementCreator, (state) => state + 1),
+  on(decrementCreator, (state) => state - 1),
+  on(resetCreator, (state) => 0)
 );
 
 @Component({
   standalone: false,
-
   templateUrl: './ngrx-spielwiese.component.html',
   styleUrl: './ngrx-spielwiese.component.css',
 })
 export class NgrxSpielwieseComponent {
   count$: Observable<number> = of(0);
 
-  constructor() {
-    // this.count$ = store.select('count');
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select('count');
   }
 
   increment() {
-    //
+    const actionIncrement = incrementCreator();
+    this.store.dispatch(actionIncrement);
   }
   decrement() {
+    this.store.dispatch(decrementCreator());
     //
   }
   reset() {
-    //
+    this.store.dispatch(resetCreator());
   }
 }
